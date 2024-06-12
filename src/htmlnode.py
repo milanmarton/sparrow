18abe5537fb8f46f1bde6a7404e61cdeb1e20fb0
milanmarton <martonmilan@proton.me>
2024-06-12 04:48:17 +0200

    added ParentNode class and unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,8 +2,8 @@ class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None) -> None:
         self.tag = tag
         self.value = value
-        self.children = children
-        self.props = props
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
 
     def to_html(self) -> str:
         raise NotImplementedError
@@ -19,15 +19,31 @@ class HTMLNode:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None) -> None:
-        if value is None:
-            raise ValueError
+    def __init__(self, tag, value, props=None) -> None:
         super().__init__(tag, value, None, props)
 
     def props_to_html(self) -> str:
         return super().props_to_html()
 
     def to_html(self) -> str:
-        if self.tag is None and self.value is not None:
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
             return self.value
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("ParentNode without tag error")
+        if self.children is None:
+            raise ValueError("ParentNode without children error")
+
+        children_html = ''
+        for child in self.children:
+            children_html = children_html + ''.join(child.to_html())
+      
+        return f'<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,19 +1,19 @@
 import unittest
-import htmlnode
+from htmlnode import (HTMLNode, LeafNode, ParentNode)
 
 class TestHTMLNode(unittest.TestCase):
     def test_constructor(self):
-        node = htmlnode.HTMLNode("p", "this is a html node", None,\
+        node = HTMLNode("p", "this is a html node", None,\
                 {"href": "https://www.google.com", "target": "_blank"})
         # python uses '' as the default string representation
-        node_as_str = "HTMLNode(p, this is a html node, None, {'href': 'https://www.google.com', 'target': '_blank'})"
+        node_as_str = "HTMLNode(p, this is a html node, [], {'href': 'https://www.google.com', 'target': '_blank'})"
         self.assertEqual(node_as_str, repr(node))
 
     def test_props_to_html(self):
         props1 = {"href": "https://www.google.com", "target": "_blank"}
         props2 = {"href": "https://www.kagi.com", "target": "_blank"}
-        node1 = htmlnode.HTMLNode(None, None, None, props1)
-        node2 = htmlnode.HTMLNode(None, None, None, props2)
+        node1 = HTMLNode(None, None, None, props1)
+        node2 = HTMLNode(None, None, None, props2)
         props_to_str_1 = ' href="https://www.google.com" target="_blank"'
         props_to_str_2 = ' href="https://www.kagi.com" target="_blank"'
 
@@ -25,13 +25,9 @@ class TestHTMLNode(unittest.TestCase):
         )
 
 class TestLeafNode(unittest.TestCase):
-    def test_constructor_error(self):
-        with self.assertRaises(ValueError):
-            node = htmlnode.LeafNode("p")
-
     def test_render(self):
-        node1 = htmlnode.LeafNode("p", "This is a paragraph of text.")
-        node2 = htmlnode.LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node1 = LeafNode("p", "This is a paragraph of text.")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         html_rendered1 = '<p>This is a paragraph of text.</p>'
         html_rendered2 = '<a href="https://www.google.com">Click me!</a>'
         self.assertEqual(
@@ -40,6 +36,59 @@ class TestLeafNode(unittest.TestCase):
         self.assertEqual(
             html_rendered2, node2.to_html()
         )
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        node = ParentNode(
+                "p",
+                [
+                    LeafNode("b", "Bold text"),
+                    LeafNode(None, "Normal text"),
+                    LeafNode("i", "italic text"),
+                    LeafNode(None, "Normal text"),
+                ],
+            )
+        html_rendered = '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+
+        self.assertEqual(
+            html_rendered, node.to_html()
+        )
+
+    def test_nested_parent_nodes(self):
+        child1 = LeafNode("span", "Child 1")
+        child2 = LeafNode("span", "Child 2")
+        inner_parent = ParentNode("div", [child1, child2], {"class": "inner-parent"})
+
+        child3 = LeafNode("p", "Outer child 1")
+        outer_parent = ParentNode("section", [inner_parent, child3], {"class": "outer-parent"})
+
+        html_rendered = '<section class="outer-parent"><div class="inner-parent"><span>Child 1</span><span>Child 2</span></div><p>Outer child 1</p></section>'
+        self.assertEqual(html_rendered, outer_parent.to_html())
+
+    def test_deeply_nested_parent_nodes(self):
+        child1 = LeafNode("span", "Child 1")
+        child2 = LeafNode("span", "Child 2")
+        inner_parent1 = ParentNode("div", [child1, child2], {"class": "inner-parent-1"})
+
+        child3 = LeafNode("span", "Child 3")
+        child4 = LeafNode("span", "Child 4")
+        inner_parent2 = ParentNode("div", [child3, child4], {"class": "inner-parent-2"})
+
+        middle_parent = ParentNode("article", [inner_parent1, inner_parent2], {"class": "middle-parent"})
+
+        outer_child = LeafNode("p", "Outer child")
+        outer_parent = ParentNode("section", [middle_parent, outer_child], {"class": "outer-parent"})
+
+        html_rendered = (
+            '<section class="outer-parent">'
+            '<article class="middle-parent">'
+            '<div class="inner-parent-1"><span>Child 1</span><span>Child 2</span></div>'
+            '<div class="inner-parent-2"><span>Child 3</span><span>Child 4</span></div>'
+            '</article>'
+            '<p>Outer child</p>'
+            '</section>'
+        )
+        self.assertEqual(html_rendered, outer_parent.to_html())
 
 if __name__ == "__main__":
     unittest.main()
