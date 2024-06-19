c02e626f05dc1bd02a06f59211ef1cbd26b6c63e
milanmarton <martonmilan@proton.me>
2024-06-20 00:04:44 +0200

    wrote functions to convert md files to html with a template

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from os import (
     mkdir,
 )
 import shutil
+from block_markdown import (
+    markdown_to_htmlnode,
+)
 
 def copy_fulldir(src: str, dst: str) -> None:
     if not path.exists(src):
@@ -31,9 +34,30 @@ def extract_title(markdown: str) -> str:
                 raise SyntaxError('There might be an empty h1 heading in your markdown')
             title_count += 1
             title = parts[1]
-    if title_count != 0 or title is None:
-        raise ValueError('There is no h1 header or there are more than one h1 header!')
+    if title_count != 1 or title is None:
+        raise SyntaxError('There is no h1 header or there are more than one h1 header!')
     return title
 
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')   
+
+    with open(from_path, 'r') as md:
+        md_contents = md.read()
+    with open(template_path, 'r') as tp:
+        tp_contents = tp.read()
+
+    html = markdown_to_htmlnode(md_contents).to_html()
+    title = extract_title(md_contents)
+
+    html_file = tp_contents.replace('{{ Title }}', title).replace('{{ Content }}', html)
+    if not path.exists(dest_path):
+        mkdir(dest_path)
+    with open(dest_path, 'w') as dst:
+        dst.write(html_file)
+
+    shutil.copy2('./index.css', './public/index.css')
+    copy_fulldir('./images', './public/images')
+
 if __name__ == '__main__':
-    copy_fulldir('./static', './public')
+    # copy_fulldir('./static', './public')
+    generate_page('./content/index.md', './template.html', './public/index.html')
