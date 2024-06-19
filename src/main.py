f13a404728ececfdd74fd166859810be904a8960
milanmarton <martonmilan@proton.me>
2024-06-19 22:02:21 +0200

    added copy_fulldir func to copy dirs recursively

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 from textnode import TextNode
+from os import (
+    path,
+    listdir,
+    mkdir,
+)
+import shutil
 
-example = TextNode("Example text node", "bold", "kagi.com")
+def copy_fulldir(src: str, dst: str) -> None:
+    if not path.exists(src):
+        raise OSError('Source directory not found')
+    if not path.exists(dst):
+        mkdir(dst)
 
-print(example)
+    for item in listdir(src):
+        src_item = path.join(src, item) # get source path of file/dir
+        dst_item = path.join(dst, item) # get destination path of file/dir
+
+        if path.isdir(src_item):
+            copy_fulldir(src_item, dst_item) # call the function recursively
+        else:
+            shutil.copy2(src_item, dst_item) # copy if item is a file
+
+
+if __name__ == '__main__':
+    copy_fulldir('./static', './public')
